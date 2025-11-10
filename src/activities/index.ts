69c9d1acc28a3d2b63927145b6ae08b2f3871dea
tom-major <tom-major>
2025-11-10 20:55:54 +0300

    Update logs

--- src/activities/index.ts
+++ src/activities/index.ts
@@ -1,29 +1,20 @@
-import { log } from '../lib/logger.js'
 import { collectTransfersActivity } from './collectTransfers.js'
 import {enrichWithReceiptsActivity} from "./enrichWithReceipts.js"
 import {computeMetricsActivity} from "./computeMetrics.js";
 import {exportJsonActivity} from "./exportJson.js";
 
 export async function collectTransfers(): Promise<void> {
-    log('[Activity] collectTransfers() started')
     await collectTransfersActivity()
-    log('[Activity] collectTransfers() finished')
 }
 
 export async function enrichWithReceipts(): Promise<void> {
-    log('[Activity] enrichWithReceipts() started')
     await enrichWithReceiptsActivity()
-    log('[Activity] enrichWithReceipts() finished')
 }
 
 export async function computeMetrics(): Promise<void> {
-    log('[Activity] computeMetrics() started')
     await computeMetricsActivity()
-    log('[Activity] computeMetrics() finished')
 }
 
 export async function exportJson(): Promise<void> {
-    log('[Activity] exportJson() started')
     await exportJsonActivity()
-    log('[Activity] exportJson() finished')
 }
